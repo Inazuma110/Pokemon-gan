026fbaaed2fde6999d7847522362d6a5a2fac75e
Inazuma110 <c011703534@edu.teu.ac.jp>
2020-01-10 14:40:17 +0900

    Add: cgan and vae

--- README.md
+++ README.md
@@ -3,13 +3,27 @@ Generate Pokemon using some GANs.
 
 # Result
 ## DCGAN
-![result1](./result/dcgan/0000015003.png)
-![result1](./result/dcgan/0000014970.png)
-
-## CGAN
+![dcgan1](./result/dcgan/0000015003.png)
+![dcgan2](./result/dcgan/0000014970.png)
 
 ## VAE
+![vae](./result/vae/vae_sample.png)
+
+## CGAN
+![cgan](./result/cgan/15100.png)
 
 
 # Usage
-1. Collect Pokemon image 64x64x3.
+1. `cd Pokemon-gan`
+1. Collect 64x64x3 Pokemon image into `poke64` directory.
+
+## DCGAN
+1. `pipenv run python3 ./dcgan.py`
+
+## VAE
+1. `pipenv run python3 ./vae/vae.py`
+
+## CGAN
+1. `cd cgan/`
+1. Create `./cgan/info.pickle`. This file has image path and Pokemon's types as `pd.DataFrame`.
+1. `pipenv run python3 cgan.py`
---
+++ cgan/cgan.py
@@ -0,0 +1,245 @@
+# MIT License
+#
+# Copyright (c) 2017 Erik Linder-Norén
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import glob
+import numpy as np
+from keras.preprocessing.image import load_img,img_to_array
+from tqdm import tqdm
+from tqdm import tqdm_notebook
+import pandas as pd
+
+df_infos = pd.read_pickle('./info.pickle')
+
+#image  array size
+img_size = (128,128)
+#load images Folder
+dir_name = 'img'
+#File type
+file_type  = 'png'
+
+#load images and image to array
+img_list = glob.glob('./' + dir_name + '/*.' + file_type)
+
+temp_img_array_list = []
+types = []
+
+for info in tqdm(df_infos):
+    temp_img = load_img(df_infos[info]['path'], grayscale=False,target_size=(img_size))
+    temp_img_array = img_to_array(temp_img) / 255
+    temp_img_array = (temp_img_array * 2) - 1
+    temp_img_array_list.append(temp_img_array)
+
+    type_tmp = np.zeros(19)
+    type_tmp[df_infos[info]['type1']] = 1
+    type_tmp[df_infos[info]['type2']] = 1
+    types.append(type_tmp)
+
+types = np.asarray(types)
+temp_img_array_list = np.array(temp_img_array_list)
+
+# from __future__ import print_function, division
+
+from keras.datasets import mnist
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout, multiply
+from keras.layers import BatchNormalization, Activation, Embedding, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+class CGAN():
+    def __init__(self):
+        # Input shape
+        self.img_rows = 128
+        self.img_cols = 128
+        self.channels = 3
+        self.img_shape = (self.img_rows, self.img_cols, self.channels)
+        self.num_classes = 19
+        self.latent_dim = 100
+
+        optimizer = Adam(0.0002, 0.5)
+
+        # Build and compile the discriminator
+        self.discriminator = self.build_discriminator()
+        self.discriminator.compile(loss=['binary_crossentropy'],
+            optimizer=optimizer,
+            metrics=['accuracy'])
+
+        # Build the generator
+        self.generator = self.build_generator()
+
+        # The generator takes noise and the target label as input
+        # and generates the corresponding digit of that label
+        noise = Input(shape=(self.latent_dim,))
+        label = Input(shape=(1,))
+        img = self.generator([noise, label])
+
+        # For the combined model we will only train the generator
+        self.discriminator.trainable = False
+
+        # The discriminator takes generated image as input and determines validity
+        # and the label of that image
+        valid = self.discriminator([img, label])
+
+        # The combined model  (stacked generator and discriminator)
+        # Trains generator to fool discriminator
+        self.combined = Model([noise, label], valid)
+        self.combined.compile(loss=['binary_crossentropy'],
+            optimizer=optimizer)
+
+    def build_generator(self):
+
+        model = Sequential()
+
+        model.add(Dense(256, input_dim=self.latent_dim))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(512))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(1024))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(np.prod(self.img_shape), activation='tanh'))
+        model.add(Reshape(self.img_shape))
+
+        model.summary()
+
+        noise = Input(shape=(self.latent_dim,))
+        label = Input(shape=(1,), dtype='int32')
+        label_embedding = Flatten()(Embedding(self.num_classes, self.latent_dim)(label))
+
+        model_input = multiply([noise, label_embedding])
+        img = model(model_input)
+
+        return Model([noise, label], img)
+
+    def build_discriminator(self):
+
+        model = Sequential()
+
+        model.add(Dense(512, input_dim=np.prod(self.img_shape)))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dense(512))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.4))
+        model.add(Dense(512))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.4))
+        model.add(Dense(1, activation='sigmoid'))
+        model.summary()
+
+        img = Input(shape=self.img_shape)
+        label = Input(shape=(1,), dtype='int32')
+
+        label_embedding = Flatten()(Embedding(self.num_classes, np.prod(self.img_shape))(label))
+        flat_img = Flatten()(img)
+
+        model_input = multiply([flat_img, label_embedding])
+
+        validity = model(model_input)
+
+        return Model([img, label], validity)
+
+    def train(self, epochs, batch_size=128, sample_interval=50):
+
+        # Load the dataset
+        (X_train, y_train), (_, _) = mnist.load_data()
+        X_train = temp_img_array_list
+        y_train = types
+
+        # Configure input
+        X_train = (X_train.astype(np.float32) - 127.5) / 127.5
+        # X_train = np.expand_dims(X_train, axis=3)
+        y_train = y_train.reshape(-1, 1)
+        print(y_train.shape)
+
+        # Adversarial ground truths
+        valid = np.ones((batch_size, 1))
+        fake = np.zeros((batch_size, 1))
+
+        for epoch in range(epochs):
+
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+
+            # Select a random half batch of images
+            idx = np.random.randint(0, X_train.shape[0], batch_size)
+            imgs, labels = X_train[idx], y_train[idx]
+
+            # Sample noise as generator input
+            noise = np.random.normal(0, 1, (batch_size, 100))
+
+            # Generate a half batch of new images
+            gen_imgs = self.generator.predict([noise, labels])
+
+            # Train the discriminator
+            d_loss_real = self.discriminator.train_on_batch([imgs, labels], valid)
+            d_loss_fake = self.discriminator.train_on_batch([gen_imgs, labels], fake)
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+            # ---------------------
+            #  Train Generator
+            # ---------------------
+
+            # Condition on labels
+            sampled_labels = np.random.randint(0, self.num_classes, batch_size).reshape(-1, 1)
+
+            # Train the generator
+            g_loss = self.combined.train_on_batch([noise, sampled_labels], valid)
+
+            # Plot the progress
+            print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
+
+            # If at save interval => save generated image samples
+            if epoch % sample_interval == 0:
+                self.sample_images(epoch)
+
+    def sample_images(self, epoch):
+        r, c = 2, 5
+        noise = np.random.normal(0, 1, (r * c, 100))
+        sampled_labels = np.arange(0, self.num_classes).reshape(-1, 1)
+
+        gen_imgs = self.generator.predict([noise, sampled_labels])
+
+        # Rescale images 0 - 1
+        gen_imgs = 0.5 * gen_imgs + 0.5
+
+        fig, axs = plt.subplots(r, c)
+        cnt = 0
+        for i in range(r):
+            for j in range(c):
+                axs[i,j].imshow(gen_imgs[cnt,:,:,0], cmap='gray')
+                axs[i,j].set_title("Digit: %d" % sampled_labels[cnt])
+                axs[i,j].axis('off')
+                cnt += 1
+        fig.savefig("res/%d.png" % epoch)
+        plt.close()
+
+
+if __name__ == '__main__':
+    cgan = CGAN()
+    cgan.train(epochs=40000, batch_size=32, sample_interval=100)
